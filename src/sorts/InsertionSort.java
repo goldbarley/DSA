6419637b11ef71e471f1660110a1c894792f13dd
Nilabhro <98198437+goldbarley@users.noreply.github.com>
2024-07-03 17:16:52 +0530

    Improved sorting algorithms.

--- src/sorts/InsertionSort.java
+++ src/sorts/InsertionSort.java
@@ -1,38 +1,7 @@
 package sorts;
-import java.util.Random;
-import java.util.Arrays;
 
 public class InsertionSort 
 {
-    public static void main(String[] args)
-    {
-        long startTime = System.nanoTime();
-
-        notes.insertionsort.printNotes();
-
-        Random r = new Random();
-        
-        int size = 100;
-        int array[] = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = r.nextInt(100);
-        }
-        System.out.println("Size of the array: " + size + "\n");
-
-        System.out.println("Before sorting:");
-        System.out.println(Arrays.toString(array));
-
-        insertionSort(array);
-
-        System.out.println("\nAfter sorting:");
-        System.out.println(Arrays.toString(array));
-
-        long endTime = System.nanoTime();
-
-        System.out.printf("Time of execution: %.4f seconds", (double) (endTime - startTime) / 1_000_000_000.0);
-    }
-
     public static void insertionSort(int[] array)
     {
         int size = array.length;
--- src/sorts/Quicksort.java
+++ src/sorts/Quicksort.java
@@ -1,39 +1,7 @@
 package sorts;
 
-import java.util.Random;
-import java.util.Arrays;
-
 public class Quicksort 
 {
-    public static void main(String[] args) 
-    {
-        long startTime = System.nanoTime();
-
-        notes.quicksort.printNotes();
-
-        Random r = new Random();
-        
-        int size = 100;
-        int array[] = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = r.nextInt(100) + 1;
-        }
-        System.out.println("Size of the array: " + size + "\n");
-
-        System.out.println("Before sorting:");
-        System.out.println(Arrays.toString(array));
-
-        quicksort(array);
-
-        System.out.println("\nAfter sorting:");
-        System.out.println(Arrays.toString(array));
-
-        long endTime = System.nanoTime();
-
-        System.out.printf("\nTime of execution: %.4f seconds", (double) (endTime - startTime) / 1_000_000_000.0);
-    }
-
     public static void quicksort(int[] array)
     {
         quicksort(array, 0, array.length - 1);
@@ -46,7 +14,7 @@ public class Quicksort
             return;
         }
 
-        int pivotIndex = new Random().nextInt(end - start) + start;
+        int pivotIndex = (int) (Math.random() * (end - start)) + start;
         int pivot = array[pivotIndex];
         swap(array, pivotIndex, end);
         
