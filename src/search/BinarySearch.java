d2e8c2648e3640703e8ea135b20c0ace9fba1458
Nilabhro <98198437+goldbarley@users.noreply.github.com>
2024-07-03 17:18:15 +0530

    Removed main function.

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -1,39 +1,8 @@
 package search;
 
-import java.util.Arrays;
-import sorts.*;
-
 public class BinarySearch 
 {
-    public static void main(String[] args)
-    {
-        long startTime = System.nanoTime();
-
-        notes.binarysearch.printNotes();
-
-        int[] array = {34, 7, 23, 32, 5, 62, 78, 45, 21, 17, 91, 54, 83, 29, 68};
-        int targetElement = 17;
-
-        Quicksort.quicksort(array);
-
-        System.out.println("Sorted array:\n" + Arrays.toString(array) + "\n");
-
-        int targetIndex = binarySearch(targetElement, array);
-        if (targetIndex >= 0)
-        {
-            System.out.printf("Element '%d' found at index %d of the sorted array.\n", targetElement, targetIndex);
-        }
-        else
-        {
-            System.out.printf("Element '%d' not found.\n", targetElement);
-        }
-
-        long endTime = System.nanoTime();
-
-        System.out.printf("\nTime of execution: %.4f seconds", (double) (endTime - startTime) / 1_000_000_000.0);
-    }    
-
-    public static int binarySearch(int targetElement, int[] array)
+    public static int binarySearch(int[] array, int targetElement)
     {
         int lowIndex = 0, highIndex = array.length - 1;
 
--- src/search/InterpolationSearch.java
+++ src/search/InterpolationSearch.java
@@ -1,39 +1,8 @@
 package search;
 
-import java.util.Arrays;
-import sorts.*;
-
 public class InterpolationSearch
 {          
-    public static void main(String[] args)
-    {
-        long lowIndexTime = System.nanoTime();
-
-        notes.interpolsearch.printNotes();
-
-        int[] array = {34, 7, 23, 32, 5, 62, 78, 45, 21, 17, 91, 54, 83, 29, 68};
-        int targetElement = 7;
-
-        Quicksort.quicksort(array);
-
-        System.out.println("Sorted array:\n" + Arrays.toString(array) + "\n");
-
-        int targetIndex = interpolationSearch(targetElement, array);
-        if (targetIndex >= 0)
-        {
-            System.out.printf("Element '%d' found at index %d of the sorted array.\n", targetElement, targetIndex);
-        }
-        else
-        {
-            System.out.printf("Element '%d' not found.\n", targetElement);
-        }
-
-        long highIndexTime = System.nanoTime();
-
-        System.out.printf("\nTime of execution: %.4f seconds", (double) (highIndexTime - lowIndexTime) / 1_000_000_000.0);
-    }    
-
-    public static int interpolationSearch(int targetElement, int[] array)
+    public static int interpolationSearch(int[] array, int targetElement)
     {
         int lowIndex = 0, highIndex = array.length - 1;
 
