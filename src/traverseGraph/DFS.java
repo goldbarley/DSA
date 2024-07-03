e25a4d1b532afcb87d19ffe44c41f3780f78c4ee
Nilabhro <98198437+goldbarley@users.noreply.github.com>
2024-07-03 17:18:40 +0530

    Removed main function.

--- src/traverseGraph/DFS.java
+++ src/traverseGraph/DFS.java
@@ -1,39 +1,19 @@
 package traverseGraph;
 
-import java.util.Random;
+import dataStructs.Graph;
 
 public class DFS 
 {
-    private static final int SIZE_OF_GRAPH = 5;
-    public static void main(String[] arGs)
+    public static void dfs(Graph G, int src)
     {
-        notes.dfs.printNotes();
-
-        long startTime = System.nanoTime();
-
-        Graph G = new Graph(SIZE_OF_GRAPH);
-
-        System.out.println("Adjacency Matrix:");
-        G.printAdjMatrix();
-
-        System.out.print("Order: ");
-        dfs(G);
-
-        System.out.println();
-
-        long endTime = System.nanoTime();
-
-        System.out.printf("Time of execution: %.4f seconds", (double) (endTime - startTime) / 1_000_000_000.0);
-    }
-
-    public static void dfs(Graph G)
-    {
-        for (int i = new Random().nextInt(SIZE_OF_GRAPH); i < SIZE_OF_GRAPH; i++)
+        int nodes = G.adjMatrix.length;
+        while (src < nodes)
         {
-            if (!G.visited[i])
+            if (!G.visited[src])
             {
-                dfs(i, G.adjMatrix, G.visited);
+                dfs(src, G.adjMatrix, G.visited);
             }
+            src++;
         }
     }
 
@@ -42,7 +22,7 @@ public class DFS
         visited[src] = true;
         System.out.print(src + " ");
 
-        for (int i = 0; i < SIZE_OF_GRAPH; i++)
+        for (int i = 0, n = matrix[src].length; i < n; i++)
         {
             if (matrix[src][i] == 1 && !visited[i])
             {
