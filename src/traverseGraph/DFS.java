f15de626da805d8bdc9149b36e4a03f8e0edb8b3
nilabhro <nilabhromondal2007@gmail.com>
2024-07-07 23:51:00 +0530

    style changes
    now uses k&r style

--- src/traverseGraph/DFS.java
+++ src/traverseGraph/DFS.java
@@ -2,34 +2,27 @@ package traverseGraph;
 
 import dataStructs.Graph;
 
-public class DFS 
-{
-    public static void dfs(Graph G, int src)
-    {
+public class DFS {
+    public static void dfs(Graph G, int src) {
         int nodes = G.adjMatrix.length;
-        while (src < nodes)
-        {
-            if (!G.visited[src])
-            {
+        while (src < nodes) {
+            if (!G.visited[src]) {
                 dfs(src, G.adjMatrix, G.visited);
             }
             src++;
         }
     }
 
-    private static void dfs(int src, int[][] matrix, boolean[] visited)
-    {
+    private static void dfs(int src, int[][] matrix, boolean[] visited) {
         visited[src] = true;
         System.out.print(src + " ");
 
-        for (int i = 0, n = matrix[src].length; i < n; i++)
-        {
-            if (matrix[src][i] == 1 && !visited[i])
-            {
+        for (int i = 0, n = matrix[src].length; i < n; i++) {
+            if (matrix[src][i] == 1 && !visited[i]) {
                 dfs(i, matrix, visited);
             }
         }
-        
+
         return;
     }
 }
