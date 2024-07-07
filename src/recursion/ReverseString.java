2ac3389837fe6a76661d7c9665738c8600a00cf8
nilabhro <nilabhromondal2007@gmail.com>
2024-07-07 20:52:43 +0530

    recursion 2
    added recursive function for checking a palindromic string

--- src/recursion/ReverseString.java
+++ src/recursion/ReverseString.java
@@ -2,14 +2,6 @@ package recursion;
 
 public class ReverseString
 {
-    public static void main(String[] args)
-    {
-        String str = "hello world";
-
-        System.out.println(str);
-        System.out.println(reverse(str));
-    }
-
     public static String reverse(String s)
     {
         if (s.length() == 1)
