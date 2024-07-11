310fab65a2a1282f74f06e5041df2ee549f928fb
nilabhro <nilabhromondal2007@gmail.com>
2024-07-11 13:42:37 +0530

    changes to interpolation search

--- Java/src/search/InterpolationSearch.java
+++ Java/src/search/InterpolationSearch.java
@@ -4,8 +4,8 @@ public class InterpolationSearch {
     public static int interpolationSearch(int[] array, int targetElement) {
         int lowIndex = 0, highIndex = array.length - 1;
 
-        while (lowIndex <= highIndex) {
-            int position = lowIndex + ((targetElement - array[lowIndex]) / (array[highIndex] - array[lowIndex])) * (highIndex - lowIndex);
+        while (lowIndex <= highIndex && targetElement >= array[lowIndex] && targetElement <= array[highIndex]) {
+            int position = lowIndex + (int) ((double) ((targetElement - array[lowIndex]) / (array[highIndex] - array[lowIndex])) * (highIndex - lowIndex));
             
             if (array[position] < targetElement) {
                 lowIndex = position + 1;
